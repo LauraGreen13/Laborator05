2183a98f69d725de028f2639ceeecfcdba9c1da0
LauraGreen13 <laura_ionel@yahoo.com>
2016-03-27 21:56:36 +0300

    a

--- labtasks/eclipse/StartedService/StartedServiceActivity/src/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
+++ labtasks/eclipse/StartedService/StartedServiceActivity/src/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
@@ -1,12 +1,16 @@
 package ro.pub.cs.systems.eim.lab05.startedserviceactivity.view;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import ro.pub.cs.systems.eim.lab05.startedserviceactivity.R;
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
 
 public class StartedServiceActivity extends Activity {
 	
@@ -17,35 +21,55 @@ public class StartedServiceActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d("StartedServiceActivity", "onCreate()");
 		setContentView(R.layout.activity_started_service);
 		
         messageTextView = (TextView)findViewById(R.id.message_text_view);
 
         // TODO: exercise 7a - create an instance of the StartedServiceBroadcastReceiver
+		startedServiceBroadcastReceiver = new StartedServiceBroadcastReceiver(messageTextView);
+
+
+
+
 
         // TODO: exercise 7b - create an instance of the IntentFilter
         // with the corresponding actions of the broadcast intents
+		startedServiceIntentFilter = new IntentFilter();
+		startedServiceIntentFilter.addAction(Constants.ACTION_STRING);
+		startedServiceIntentFilter.addAction(Constants.ACTION_INTEGER);
+		startedServiceIntentFilter.addAction(Constants.ACTION_ARRAY_LIST);
 
         // TODO: exercise 7d - start the service
+		Intent intent = new Intent();
+		intent.setComponent(new ComponentName("ro.pub.cs.systems.eim.lab05.startedservice",
+				"ro.pub.cs.systems.eim.lab05.startedservice.service.StartedService"));
+		startService(intent);
+		startService(intent);
 	}
 
     @Override
     protected void onResume() {
-        super.onResume();
+		super.onResume();
 
         // TODO: exercise 7c - register the broadcast receiver for the intent filter actions
+		registerReceiver(startedServiceBroadcastReceiver, startedServiceIntentFilter);
     }
 
     @Override
     protected void onPause() {
         // TODO: exercise 7c - unregister the broadcast receiver
-
+		unregisterReceiver(startedServiceBroadcastReceiver);
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
         // TODO: exercise 7d - stop the service
+		Intent intent = new Intent();
+		intent.setComponent(new ComponentName("ro.pub.cs.systems.eim.lab05.startedservice.service",
+				"ro.pub.cs.systems.eim.lab05.startedservice.service.StartedService"));
+		stopService(intent);
 
         super.onDestroy();
     }
--- labtasks/eclipse/StartedService/StartedServiceActivity/src/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
+++ labtasks/eclipse/StartedService/StartedServiceActivity/src/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
@@ -3,13 +3,20 @@ package ro.pub.cs.systems.eim.lab05.startedserviceactivity.view;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
 
 public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
 
     private TextView messageTextView;
 
     // TODO: exercise 8 - default constructor
+
+    public StartedServiceBroadcastReceiver() {
+        this.messageTextView = null;
+    }
 
     public StartedServiceBroadcastReceiver(TextView messageTextView) {
         this.messageTextView = messageTextView;
@@ -20,8 +27,39 @@ public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
         // TODO: exercise 6 - get the action and the extra information from the intent
         // and set the text on the messageTextView
 
+        String data = null;
+        String action = intent.getAction();
+
+        Log.d("SHIT", action);
+
+        if (Constants.ACTION_STRING.equals(action)) {
+            data = intent.getStringExtra(Constants.DATA);
+        }
+        if (Constants.ACTION_INTEGER.equals(action)) {
+            data = String.valueOf(intent.getIntExtra(Constants.DATA, -1));
+        }
+        if (Constants.ACTION_ARRAY_LIST.equals(action)) {
+            data = intent.getStringArrayListExtra(Constants.DATA).toString();
+        }
+
+        if (messageTextView != null) {
+
+            if (data != null) {
+                messageTextView.setText(messageTextView.getText().toString() + "\n" + data);
+            } else {
+                messageTextView.setText("SHIT");
+            }
+
+        }
+
+
         // TODO: exercise 8 - restart the activity through an intent
         // if the messageTextView is not available
+        Intent intent1 = new Intent(context, StartedServiceActivity.class);
+        intent1.putExtra(Constants.MESSAGE, data);
+        intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(intent1);
+
     }
 
 }
